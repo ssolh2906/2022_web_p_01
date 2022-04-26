b61574a41ceea627339f5e7e33ff0b3b30037a79
Solhee Park <solheepark29@gmail.com>
2022-04-26 13:40:51 +0900

    [minor fix] url modified

--- p02/blog/urls.py
+++ p02/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.post_list, name='post_list'),  # post_views 라는 view가 ^$(^로 시작해 $로 끝나는 것) 에 할당
+    path('', views.post_list, name='post_list'),
+    #path(r'^$', views.post_list, name='post_list'),  # post_views 라는 view가 ^$(^로 시작해 $로 끝나는 것) 에 할당
 ]
