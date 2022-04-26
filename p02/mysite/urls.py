bfb2030b82267d6863d2686cc469d60f4fee2b37
Solhee Park <solheepark29@gmail.com>
2022-04-26 13:34:05 +0900

    [p05] Model and Database

--- p02/mysite/urls.py
+++ p02/mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path(r'', include('blog.urls')),
 ]
