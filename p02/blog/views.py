d6ab395b7ac533af2a05bd516d6894557ea16fb3
Solhee Park <solheepark29@gmail.com>
2022-05-03 12:53:50 +0900

    [p06] HTML5 document structure

--- p02/blog/views.py
+++ p02/blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/p06_self_portrait.html', {})
