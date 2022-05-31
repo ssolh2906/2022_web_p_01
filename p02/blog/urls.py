1c7d9b97012bea3905ce6a29b49927540359b2ec
Solhee Park <solheepark29@gmail.com>
2022-05-31 14:10:18 +0900

    [p09] Extend application

--- p02/blog/urls.py
+++ p02/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     #path(r'^$', views.post_list, name='post_list'),  # post_views 라는 view가 ^$(^로 시작해 $로 끝나는 것) 에 할당
 ]
